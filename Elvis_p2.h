60beefed96e1a9abc29a8c04e77e9bb05f00a542
engineerIOT <46654184+engineerIOT@users.noreply.github.com>
2020-08-23 13:52:10 -0300

    Add files via upload

--- Elvis_p2.h
+++ Elvis_p2.h
@@ -1,45 +1,11 @@
-#include <iostream>
-using namespace std;
-
 class Funcao {
 public:
-	Funcao(Funcao* f) { }
-	Funcao() { cout << "(" << this << ") Funcao Constructed!" << endl; } //construtor padrao
-	virtual ~Funcao() {} //destrutor padrao
-	double operator() (double x) 	{}
-	double integrar (Funcao* f, double x0, double x1, double step){}
+	Funcao();
 
-};
-
-class Constante : public Funcao {
-public:
-	double _value;
-	Constante(double val) : _value(val) { cout << "(" << this << ") Constante(" << _value << ") Constructed!" << endl; }
 private:
-	//Constante(double val) : _value(val) { cout << "(" << this << ") Constante(" << _value << ") Constructed!" << endl; }
-	double operator()(double x) {
-		return _value;
-	}
+	Funcao* f;
+
+
 };
 
-
-
-
-/*
-class Seno : public Funcao {};
-class Coseno : public Funcao {};
-class FuncaoAgregada : public Funcao {};
-class Escalar : public Funcao {};
-class Potencial : public Funcao {};
-class Exponencial : public Funcao {};*/
-
-
-
-
-int main()
-{
-	Funcao ();
-	Constante a(5);
-	
-}
 
