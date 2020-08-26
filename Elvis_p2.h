5963a23ed768ff76d3715db57ea9db6a3d62508e
engineerIOT <46654184+engineerIOT@users.noreply.github.com>
2020-08-26 19:12:35 -0300

    Add files via upload

--- Elvis_p2.h
+++ Elvis_p2.h
@@ -1,22 +1,33 @@
 #include <iostream>
 using namespace std;
+#include <vector>
 
 class Funcao {
 public:
 	Funcao(Funcao* f) { }
-	Funcao() { cout << "(" << this << ") Funcao Constructed!" << endl; } //construtor padrao
+	Funcao() { /*cout << "(" << this << ") Funcao Constructed!" << endl;*/ } //construtor padrao
 	virtual ~Funcao() {} //destrutor padrao
-	double operator() (double x) 	{}
-	double integrar (Funcao* f, double x0, double x1, double step){}
+	double operator() (double x = 0) {
+		return x;
+	}
+	double integrar (Funcao* f, double x0, double x1, double step){
+		double _area = 0;
+		double i;
+		for (i = x0; i <= x1; i += step) {
+			cout << "f(x) = " << f->operator()(i) << "\n";
+			_area += ((f->operator()(i)) * step);
+		}
+		return (_area);
+	}
 };
 
 class Constante : public Funcao {
 private:
 	double _value;
 public:
-	Constante(double val) : _value(val) { cout << "(" << this << ") Constante(" << _value << ") Constructed!" << endl; }
-	double operator()(double x) {
-		return x;
+	Constante(double val) : _value(val) { cout << "(" << this << ") f(x)=(" << _value << ") " << endl; }
+	double operator()(double) {
+		return (_value);
 	}
 };
 
@@ -24,9 +35,15 @@ public:
 class Escalar : public Funcao {
 public:
 	Escalar (double val,Funcao *f): _value(val), f(f){}
-	Escalar	(Funcao* f){}
-	Escalar(double val) : _value(val) { cout << "(" << this << ") Escalar(" << _value << ") Constructed!" << endl; }
-	Escalar() { cout << "(" << this << ") Escalar Constructed!" << endl; } //construtor padrao
+	Escalar(Funcao* f) : f(f){ }
+	Escalar(double val) : _value(val) { cout << "(" << this << ") g(x)=(" << _value << ") " << endl; }
+	Escalar() { /*cout << "(" << this << ") Escalar Constructed!" << endl;*/ } //construtor padrao
+	double operator()(double x) {
+		double _escalar = 0;
+		_escalar = (*f)(x);
+		_escalar = _escalar * _value;
+		return (_escalar);
+	}
 private:
 	double _value;
 	Funcao* f;
@@ -36,11 +53,16 @@ private:
 class Potencial : public Funcao {
 public:
 	Potencial(double val, Funcao *f): _value(val), f(f){}
-	Potencial(Funcao* f) {}
+	Potencial(Funcao* f) : f(f) {}
 	Potencial(double val) : _value(val) { cout << "(" << this << ") Potencial(" << _value << ") Constructed!" << endl; }
 	Potencial() { cout << "(" << this << ") Potencial Constructed!" << endl; } //construtor padrao
 	double operator()(double x){ 
-		return x;
+		double _potencial = x;
+		uint8_t i;
+		for (i = 0; i < (_value - 1); i++) {
+			_potencial *= x;
+		}
+		return (_potencial);
 	}
 private:
 	double _value;
@@ -50,7 +72,7 @@ private:
 class Exponencial : public Funcao {
 public:
 	Exponencial(double val, Funcao* f) : _value(val), f(f) {}
-	Exponencial(Funcao* f) {}
+	Exponencial(Funcao* f) : f(f) {}
 	Exponencial(double val) : _value(val) { cout << "(" << this << ") Exponencial(" << _value << ") Constructed!" << endl; }
 	Exponencial() { cout << "(" << this << ") Exponencial Constructed!" << endl; } //construtor padrao
 	double operator()(double x) {
@@ -87,20 +109,42 @@ public:
 
 class FuncaoAgregada : public Funcao {
 public:
-	void agrega(Funcao *f){}
-	double operator()(double x) {
-		return x;
+	void agrega(Funcao *f){
+		_myvector.push_back(f);
 	}
-
+	double operator()(const double x) {
+		double _funcaoAgregada=0;
+		for (it = _myvector.begin(); it != _myvector.end(); it++) {
+			_funcaoAgregada += static_cast<Funcao*>(*it)->operator()(x);
+		}
+		return _funcaoAgregada;
+		
+	}
+private: 
+	vector <Funcao*> _myvector;
+	vector <Funcao*> ::iterator it;
 };
 
 
 void main()
 {
 	//Funcao ();
-	//Escalar g(3, new Potencial(2));// g(x) = 3xˆ2;
-	Constante i(5);    //i(x)=5;
-	Escalar h(2);
+	
+	//Escalar g(0);    //i(x)=5;
+	Constante h(0);
+	Constante i(0);
+
+	FuncaoAgregada f;
+	f.agrega(&h); f.agrega(&i); 
+
+	double x = 0;
+	while (x < 5) {
+		cout << "f(" << x << ") = " << f(x) << endl;
+		x += 0.01;
+	}
+
+	cout << " A integral de [0,5] : ";
+	cout << f.integrar(&f, 0, 5, 0.01) << endl;
 	
 }
 
