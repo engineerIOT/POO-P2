015ac81e8fb9355414cc5d2a1e6e3abd1dbb6c27
engineerIOT <46654184+engineerIOT@users.noreply.github.com>
2020-08-27 14:13:22 -0300

    Add files via upload

--- Elvis_p2.h
+++ Elvis_p2.h
@@ -1,19 +1,19 @@
-#include <iostream>
+#include <iostream>		//biblioteca entradas e saidas
 using namespace std;
-#include <vector>
+#include <vector>		//adicionada para utilizar na funcao agrega
 
-class Funcao {
+class Funcao {				//Uma classe abstrata que serve de modelo para outras classes.
 public:
-	Funcao(Funcao* f) { }
+	Funcao(Funcao* f) { } //ponteiro para a uncao empregada (f)
 	Funcao() { /*cout << "(" << this << ") Funcao Constructed!" << endl;*/ } //construtor padrao
-	virtual ~Funcao() {} //destrutor padrao
-	double operator() (double x) {
-		return x;
+	virtual ~Funcao() { /*cout << "(" << this << ") Funcao Desconstructed!" << endl;*/ } //destrutor padrao
+	double operator() (double x) { 
+		return x; //a classe funcao retorna o valor da funcao através do operador de chamada de funcao
 	}
-	double integrar (Funcao* f, double x0, double x1, double step){
+	static double integrar (Funcao* f, double x0, double x1, double step){	// método estático 
 		double _area = 0;
 		double i;
-		for (i = x0; i <= x1; i += step) {
+		for (i = x0; i <= x1 - 1; i += step) {
 			cout << "f(x) = " << f->operator()(i) << "\n";
 			_area += ((f->operator()(i)) * step);
 		}
@@ -21,10 +21,31 @@ public:
 	}
 };
 
+
+class FuncaoAgregada : public Funcao {
+public:
+	//FuncaoAgregada() {}
+	void agrega(Funcao* f) {
+		_myvector.push_back(f);
+	}
+	double operator()(double x) {
+		double _funcaoAgregada = 0;
+		for (it = _myvector.begin(); it != _myvector.end(); it++) {
+			_funcaoAgregada += static_cast<Funcao*>(*it)->operator()(x);
+		}
+		return _funcaoAgregada;
+
+	}
+private:
+	vector <Funcao*> _myvector;
+	vector <Funcao*> ::iterator it;
+};
+
+
 class Constante : public Funcao {
 public:
 	Constante(double val) : _value(val) { cout << "(" << this << ") constante(x)=(" << _value << ") " << endl; }
-	//Constante() {}
+	Constante() {}
 	double operator()(double x) {
 		return (_value);
 	}
@@ -131,48 +152,33 @@ private:
 };
 
 
-class FuncaoAgregada : public Funcao {
-public:
-	FuncaoAgregada() {}
-	void agrega(Funcao *f){
-		_myvector.push_back(f);
-	}
-	double operator()(double x) {
-		double _funcaoAgregada= 0;
-		for (it = _myvector.begin(); it != _myvector.end(); it++) {
-			_funcaoAgregada += static_cast<Funcao*>(*it)->operator()(x);
-		}
-		return _funcaoAgregada;
-		
-	}
-private: 
-	vector <Funcao*> _myvector;
-	vector <Funcao*> ::iterator it;
-};
+
 
 
 void main()
 {
 	
-	Escalar g(3, new Potencial (2)); //g(x) = 3x^2
-	Constante h(2);
-	Escalar i(2);
-	
+	//Escalar g(1, new Potencial (1)); //g(x) = 3x^2
+	//Escalar h(3);
+	//Constante i(8);
+	//Escalar i(3);
+	Constante h(3);
+	Constante i(3);
 
 
 	FuncaoAgregada f;
-	f.agrega(&g); 
+	//f.agrega(&g); 
 	f.agrega(&h); 
-	//f.agrega(&i);
+	f.agrega(&i);
 
 	double x = 0;
 	while (x < 5) {
 		cout << "f(" << x << ") = " << f(x) << endl;
-		x += 0.01;
+		x += 1;
 	}
 
 	cout << " A integral de [0,5] : ";
-	cout << f.integrar(&f, 0, 5, 0.01) << endl;
+	cout << f.integrar(&f, 0, 5, 1) << endl;
 	
 }
 
